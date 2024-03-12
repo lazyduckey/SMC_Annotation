b38bf3d7d41fd5f3771b3fed6dc1dbe52da4b5bd
lazyduckey <psm6932@naver.com>
2024-03-12 14:50:59 +0900

    edit task and step list and add find index function of task and step

--- utils/util.py
+++ utils/util.py
@@ -20,3 +20,16 @@ def get_index_action(selected_clip_info, role, hand, action):
         act_idx = 2
         
     return action_triplet_dict[action].index(selected_clip_info[role_idx].split('\n')[hand_idx].split(',')[act_idx]) if selected_clip_info[role_idx] else 0
+
+
+def find_index(selected_clip_info, task_or_step_lst, idx):
+    if selected_clip_info[idx]:
+        if selected_clip_info[idx] in task_or_step_lst:
+            return task_or_step_lst.index(selected_clip_info[idx])
+        else:
+            return selected_clip_info[-1]
+    else:    
+        return None
+    
+    
+    
